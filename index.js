38c855fed8a1fe33c3cf97c238d33b6fce702aa2
--replace-all <terrencejung928@gmail.com>
2022-05-17 20:13:41 -0700

    commit tree 92c803f7c01f67265b8b7d9881ecebbd1f8152f6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(name) {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'max';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not max';
+}
